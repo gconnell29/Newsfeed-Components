344f54b13cd1e43eeea355dcdc905cadf853e3d0
gconnell29 <gconnelljr29@gmail.com>
2021-01-22 00:11:24 -0500

    created and appended article nodes to DOM

--- components/Article.js
+++ components/Article.js
@@ -92,25 +92,43 @@ const data = [
 /*
   Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
-  and returns a DOM node looking like the one below:
+  and returns a DOM node looking like the one below: */
 
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
-
-    {three separate paragraph elements}
-
-    <span class="expandButton">+</span>
+  function articleMaker(articleObj) {
+    let article = document.createElement('div');
+    article.classList.add = ('article');
+    let h2 = document.createElement('h2');
+    let date = document.createElement('p');
+    date.classList.add('date');
+    h2.textContent = articleObj.title;
+    date.textContent = articleObj.date;
+    article.appendChild(h2);
+    article.appendChild(date);
+    return article;
+  }
+  // console.log("test")
+  /*<div class="article">
+  <h2>{title of the article}</h2>
+  <p class="date">{date of the article}</p>
+  
+  {three separate paragraph elements}
+  
+  <span class="expandButton">+</span>
   </div>
-
+  
   Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
   This listener should toggle the class 'article-open' on div.article.
-
-  Step 3: Don't forget to return something from your function!
-
-  Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
-
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  
+  Step 3: Don't forget to return something from your function!*/
+  
+  /*Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
+  to create a div.article element and append it to the DOM inside div.articles (see index.html).*/
+  
+  let articleSection = document.querySelector('.articles');
+  data.forEach(element => {
+    articleSection.appendChild(articleMaker(element));
+  });
+  
+  /*  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
